f39b509944d16521219dfd8b547492e6589b4985
kdimonn <kiiko.dmytro@gmail.com>
2018-09-10 16:31:10 +0300

    added UserHistory

--- src/main/controller/UserController.java
+++ src/main/controller/UserController.java
@@ -1,6 +1,8 @@
 package main.controller;
 
 import main.entity.User;
+import main.service.BookInstanceService;
+import main.service.impl.BookServiceImpl;
 import main.service.impl.RecordServiceImpl;
 import main.service.impl.UserServiceImpl;
 import org.hibernate.Session;
@@ -11,6 +13,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
@@ -22,6 +25,10 @@ public class UserController {
     RecordServiceImpl recordService;
     @Autowired
     UserServiceImpl userService;
+    @Autowired
+    BookServiceImpl bookService;
+    @Autowired
+    BookInstanceService bookInstanceService;
 
     @GetMapping("/me")
     public ModelAndView infoAboutMe() {
@@ -36,7 +43,21 @@ public class UserController {
         return modelAndView;
     }
     @GetMapping("/trends")
-    public String showTrends(){
-        return "user/trends";
+    public ModelAndView showTrends(@RequestParam("dropDown")String value){
+        int days = Integer.parseInt(value);
+        ModelAndView modelAndView = new ModelAndView("user/trends");
+        modelAndView.addObject("listOfPopular",bookService.getTopBooks(days));
+        modelAndView.addObject("listOfUnpopular",bookService.getFlopBooks());
+        modelAndView.addObject("numberDuringIndep",bookInstanceService.countInstancesAfter1991());
+        return modelAndView;
+    }
+    @GetMapping("/history")
+    public ModelAndView showMyHistory(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+        long id = Long.parseLong(currentPrincipalName);
+        ModelAndView modelAndView = new ModelAndView("user/history");
+        modelAndView.addObject("myRecords",recordService.getHistoryOfRecords(id));
+        return modelAndView;
     }
 }
